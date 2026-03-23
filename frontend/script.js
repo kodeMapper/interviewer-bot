78e93a5de14fe1c15a829d84fb35dea2071a9ce6
kodemapper <saranganilgade@gmail.com>
2026-03-23 20:33:07 +0530

    Evaluation-2: Re-arrangement of files

--- frontend/script.js
+++
@@ -1,9 +0,0 @@
-function startJava() {
-  fetch("http://127.0.0.1:5000/start/java");
-  alert("Java Interview Started. Wear headphones.");
-}
-
-function startDbms() {
-  fetch("http://127.0.0.1:5000/start/dbms");
-  alert("DBMS Interview Started. Wear headphones.");
-}
